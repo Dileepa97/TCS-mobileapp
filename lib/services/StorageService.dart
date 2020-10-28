03e73efb4883c8b8653ae82a06bd794b0d1c5e9e
Kalpa <kalpafernando1998@gmail.com>
2020-10-28 22:01:46 +0530

    created TokenStorageService

--- lib/services/StorageService.dart
+++ lib/services/StorageService.dart
@@ -1,1 +1,11 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+final storage = FlutterSecureStorage();
+
+class TokenStorageService {
+  static Future<String> get jwtOrEmpty async {
+    var jwt = await storage.read(key: "jwt");
+    if (jwt == null) return "";
+    return jwt;
+  }
+}
