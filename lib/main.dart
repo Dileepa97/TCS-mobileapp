87792d7b4928a7ae60e029c9cf9e01bc2c786247
Kalpa <kalpafernando1998@gmail.com>
2020-10-28 11:16:23 +0530

    initialized login and registration screens, added constants.dart to keep constant values required and modified main

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:timecapturesytem/view/Auth/auth.dart';
+import 'view/Auth/login_screen.dart';
+import 'view/Auth/registration_screen.dart';
 import 'view/LMS/user/userLeave.dart';
 import 'view/LMS/user/leaveRequest.dart';
 import 'view/homePage.dart';
 
-void main() => runApp(MaterialApp(
-      title: 'routes',
-
-      initialRoute: '/', // Start the app with the "/" named route.
-      routes: {
-        '/': (context) => HomePage(), // build the HomePage widget.
-        '/auth': (context) => AuthScreen(), // build the HomePage widget.
-        '/userLeave': (context) => UserLeave(), //  build the UserLeave widget.
-        '/leaveRequest': (context) =>
-            LeaveRequest(), //  build the LeaveRequest widget.
-      },
-    ));
+void main() => runApp(
+      MaterialApp(
+        title: 'routes',
+        initialRoute: '/', // Start the app with the "/" named route.
+        routes: {
+          '/': (context) => HomePage(),
+          // build the HomePage widget.
+          '/login': (context) => LoginScreen(),
+          // build the HomePage widget.
+          '/register': (context) => RegistrationScreen(),
+          // build the HomePage widget.
+          '/userLeave': (context) => UserLeave(),
+          //  build the UserLeave widget.
+          '/leaveRequest': (context) => LeaveRequest(),
+          //  build the LeaveRequest widget.
+        },
+      ),
+    );
