ad64007a6129f45d3a3ed5d2db0486c0f8493333
Kalpa <kalpafernando1998@gmail.com>
2020-10-29 10:40:22 +0530

    changes added to login screen

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:timecapturesystem/components/rounded_button.dart';
 import 'package:timecapturesystem/services/AuthService.dart';
 
@@ -15,59 +16,75 @@ class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final AuthService _authService = AuthService();
+  bool spin = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: spin,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              controller: _usernameController,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your username'),
-            ),
-            SizedBox(
-              height: 16.0,
-            ),
-            TextField(
-              controller: _passwordController,
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your password.'),
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            RoundedButton(
-              color: Colors.lightBlueAccent,
-              onPressed: () {
-                //implement login
-                _authService.login(
-                    _usernameController.text, _passwordController.text);
-              },
-              title: 'Login',
-            ),
-          ],
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                controller: _usernameController,
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your username'),
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              TextField(
+                controller: _passwordController,
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your password.'),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              RoundedButton(
+                color: Colors.lightBlueAccent,
+                onPressed: () async {
+                  setState(() {
+                    spin = true;
+                  });
+                  //implement login
+                  try {
+                    bool logged = await _authService.login(
+                        _usernameController.text, _passwordController.text);
+                    print(logged);
+                  } catch (e) {
+                    print(e);
+                  }
+
+                  setState(() {
+                    spin = false;
+                  });
+                },
+                title: 'Login',
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/view/auth/registration_screen.dart
+++ lib/view/auth/registration_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:timecapturesystem/components/rounded_button.dart';
+import 'package:timecapturesystem/services/AuthService.dart';
 
 import '../constants.dart';
 
@@ -12,92 +14,108 @@ class RegistrationScreen extends StatefulWidget {
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
   static const double spaceBetweenFields = 16.0;
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final AuthService _authService = AuthService();
+  bool spin = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: ListView(
-          shrinkWrap: true,
-          children: <Widget>[
-            SizedBox(
-              height: 48.0,
-            ),
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        inAsyncCall: spin,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              SizedBox(
+                height: 48.0,
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Username'),
-            ),
-            SizedBox(
-              height: spaceBetweenFields,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Full Name'),
-            ),
-            SizedBox(
-              height: spaceBetweenFields,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Email'),
-            ),
-            SizedBox(
-              height: spaceBetweenFields,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Telephone Number (optional)'),
-            ),
-            SizedBox(
-              height: spaceBetweenFields,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Password'),
-            ),
-            SizedBox(
-              height: spaceBetweenFields,
-            ),
-            TextField(
-              onChanged: (value) {
-                //Do something with the user input.
-              },
-              decoration:
-                  kTextFieldDecoration.copyWith(hintText: 'Confirm password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              color: Colors.blue,
-              onPressed: () {
-                //implement registration
-              },
-              title: 'Register',
-            ),
-          ],
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
+              ),
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Username'),
+              ),
+              SizedBox(
+                height: spaceBetweenFields,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Full Name'),
+              ),
+              SizedBox(
+                height: spaceBetweenFields,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Email'),
+              ),
+              SizedBox(
+                height: spaceBetweenFields,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Telephone Number (optional)'),
+              ),
+              SizedBox(
+                height: spaceBetweenFields,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration: kTextFieldDecoration.copyWith(hintText: 'Password'),
+              ),
+              SizedBox(
+                height: spaceBetweenFields,
+              ),
+              TextField(
+                onChanged: (value) {
+                  //Do something with the user input.
+                },
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Confirm password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                color: Colors.blue,
+                onPressed: () {
+                  setState(() {
+                    spin = true;
+                  });
+                  //implement registration
+
+                  setState(() {
+                    spin = false;
+                  });
+                },
+                title: 'Register',
+              ),
+            ],
+          ),
         ),
       ),
     );
