b25138b4528e0639efe5348f3bad933e34bc9d0f
Kalpa <kalpafernando1998@gmail.com>
2020-11-26 13:14:38 +0530

    sure dialog boxes initialized for user management functions

--- lib/components/dialog_box.dart
+++ lib/components/dialog_box.dart
@@ -44,3 +44,111 @@ void updateSuccessDialog(context) => showDialog(
         ],
       ),
     );
+
+void displayVerifySureDialog(context) => showDialog(
+      barrierColor: Colors.white70,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Registration Success"),
+        content: Text(
+            "You have successfully registered to time capture system\nPlease check your email for confirmation link"),
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.popAndPushNamed(context, LoginScreen.id);
+            },
+          )
+        ],
+      ),
+    );
+
+void displayUnVerifySureDialog(context) => showDialog(
+      barrierColor: Colors.white70,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Registration Success"),
+        content: Text(
+            "You have successfully registered to time capture system\nPlease check your email for confirmation link"),
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.popAndPushNamed(context, LoginScreen.id);
+            },
+          )
+        ],
+      ),
+    );
+
+void displayUpliftToAdminSureDialog(context) => showDialog(
+      barrierColor: Colors.white70,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Registration Success"),
+        content: Text(
+            "You have successfully registered to time capture system\nPlease check your email for confirmation link"),
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.popAndPushNamed(context, LoginScreen.id);
+            },
+          )
+        ],
+      ),
+    );
+
+void displayUpliftToTeamLeadSureDialog(context) => showDialog(
+      barrierColor: Colors.white70,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Registration Success"),
+        content: Text(
+            "You have successfully registered to time capture system\nPlease check your email for confirmation link"),
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.popAndPushNamed(context, LoginScreen.id);
+            },
+          )
+        ],
+      ),
+    );
+
+void displayDowngradeAdminSureDialog(context) => showDialog(
+      barrierColor: Colors.white70,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Registration Success"),
+        content: Text(
+            "You have successfully registered to time capture system\nPlease check your email for confirmation link"),
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.popAndPushNamed(context, LoginScreen.id);
+            },
+          )
+        ],
+      ),
+    );
+
+void displayDowngradeTeamLeadSureDialog(context) => showDialog(
+      barrierColor: Colors.white70,
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text("Registration Success"),
+        content: Text(
+            "You have successfully registered to time capture system\nPlease check your email for confirmation link"),
+        actions: [
+          FlatButton(
+            child: Text("OK"),
+            onPressed: () {
+              Navigator.popAndPushNamed(context, LoginScreen.id);
+            },
+          )
+        ],
+      ),
+    );
