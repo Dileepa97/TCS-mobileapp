464fb5bb759f1641107892f63fa01df67fa792cc
Kalpa <kalpafernando1998@gmail.com>
2020-12-02 17:15:27 +0530

    added dio package

--- lib/view/user/edit_profile_screen.dart
+++ lib/view/user/edit_profile_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:timecapturesystem/components/dialog_boxes.dart';
 import 'package:timecapturesystem/models/user/user.dart';
