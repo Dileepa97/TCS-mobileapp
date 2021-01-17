008ade6d3d203e7a32983fdc2e174a60e633a59b
Kalpa <kalpafernando1998@gmail.com>
2021-01-18 00:57:16 +0530

    added swipe down to refresh on user management dashboard

--- lib/view/user_management/user_management_dashboard_screen.dart
+++ lib/view/user_management/user_management_dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:timecapturesystem/managers/orientation.dart';
 import 'package:timecapturesystem/models/user/user.dart';
 import 'package:timecapturesystem/services/user_service.dart';
@@ -17,6 +18,26 @@ class UserManagementDashboard extends StatefulWidget {
 }
 
 class _UserManagementDashboardState extends State<UserManagementDashboard> {
+  List<User> users = [];
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: true);
+
+  void _onRefresh() async {
+    users = await UserService.getAllUsers(context);
+    if (users != [] || users.length > 0) {
+      users = users.reversed.toList();
+    }
+    if (mounted) setState(() {});
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
+  }
+
+  void _onLoading() async {
+    // if failed,use loadFailed(),if no data return,use LoadNodata()
+    if (mounted) setState(() {});
+    _refreshController.loadComplete();
+  }
+
   @override
   Widget build(BuildContext context) {
     OrientationManager.enableRotation();
@@ -30,40 +51,31 @@ class _UserManagementDashboardState extends State<UserManagementDashboard> {
       ),
       backgroundColor: Colors.lightBlue.shade800,
       body: SafeArea(
-        child: FutureBuilder<List<User>>(
-          future: UserService.getAllUsers(context),
-          builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
-            var child;
-            if (snapshot.hasData) {
-              var users = <Widget>[];
-              snapshot.data.forEach((user) {
-                users.add(UserCard(user: user));
-              });
-
-              child = ListView(
-                children: users,
-              );
-            } else if (snapshot.hasError) {
-              child = Column(children: <Widget>[
-                Icon(
-                  Icons.error_outline,
-                  color: Colors.red,
-                  size: 60,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16),
-                  child: Text('Error: ${snapshot.error}'),
-                )
-              ]);
-            } else {
-              child = Container(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return Center(child: child);
-          },
+          child: SmartRefresher(
+        enablePullDown: true,
+        header: ClassicHeader(
+          textStyle: TextStyle(color: Colors.white),
         ),
-      ),
+        controller: _refreshController,
+        onRefresh: _onRefresh,
+        onLoading: _onLoading,
+        child: users.length > 0
+            ? ListView.builder(
+                itemBuilder: (c, i) => Center(
+                  child: UserCard(user: users[i]),
+                ),
+                itemExtent: 80.0,
+                itemCount: users.length,
+              )
+            : Center(
+                child: Container(
+                  child: Text(
+                    'No new notifications available',
+                    style: TextStyle(fontSize: 20, color: Colors.white),
+                  ),
+                ),
+              ),
+      )),
     );
   }
 
@@ -73,3 +85,39 @@ class _UserManagementDashboardState extends State<UserManagementDashboard> {
     super.dispose();
   }
 }
+
+// SafeArea(
+// child: FutureBuilder<List<User>>(
+// future: UserService.getAllUsers(context),
+// builder: (BuildContext context, AsyncSnapshot<List<User>> snapshot) {
+// var child;
+// if (snapshot.hasData) {
+// var users = <Widget>[];
+// snapshot.data.forEach((user) {
+// users.add(UserCard(user: user));
+// });
+//
+// child = ListView(
+// children: users,
+// );
+// } else if (snapshot.hasError) {
+// child = Column(children: <Widget>[
+// Icon(
+// Icons.error_outline,
+// color: Colors.red,
+// size: 60,
+// ),
+// Padding(
+// padding: const EdgeInsets.only(top: 16),
+// child: Text('Error: ${snapshot.error}'),
+// )
+// ]);
+// } else {
+// child = Container(
+// child: CircularProgressIndicator(),
+// );
+// }
+// return Center(child: child);
+// },
+// ),
+// )
