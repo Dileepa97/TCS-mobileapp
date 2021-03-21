41a0d78e5cda19f367193669e06c7a6185f72004
Kalpa <kalpafernando1998@gmail.com>
2021-03-21 18:56:04 +0530

    minor change

--- lib/view/user_management/user_details_screen.dart
+++ lib/view/user_management/user_details_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:timecapturesystem/components/dialog_boxes.dart';
