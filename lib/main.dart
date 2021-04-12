41f8423c489d04749e6bcab79ce5bb37903cb0dd
Kalpa <kalpafernando1998@gmail.com>
2021-04-13 02:21:15 +0530

    removed debug badge

--- lib/main.dart
+++ lib/main.dart
@@ -170,6 +170,7 @@ class _MyAppState extends State<MyApp> {
           }
 
           return MaterialApp(
+              debugShowCheckedModeBanner: false,
               title: 'routes',
               theme: ThemeData(
                 scaffoldBackgroundColor: Color(0xFFF1F1F1),
