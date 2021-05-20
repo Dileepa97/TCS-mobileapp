0599f65496f355a3858e9f1afbeafde18499e701
ChethiyaNishanath <chethiyanishanath@gmail.com>
2021-05-20 16:11:20 +0530

    Resolved conflict

--- lib/view/task/product_list.dart
+++ lib/view/task/product_list.dart
@@ -1,83 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:timecapturesystem/models/product/product.dart';
+import 'package:timecapturesystem/services/product/product_service.dart';
 import 'package:timecapturesystem/view/side_nav/side_drawer.dart';
 import 'package:timecapturesystem/view/task/product_dashboard.dart';
 
-
-class TaskPanel extends StatelessWidget {
-  Widget productCard(String product, BuildContext context) {
-//    return InkWell(
-//      onTap: () {
-//        Navigator.pushReplacement(
-//            context,
-//            MaterialPageRoute(
-//                builder: (BuildContext context) => TaskDashboard("119")));
-//      },
-//      child: Container(
-//        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-//        child: Column(
-//          crossAxisAlignment: CrossAxisAlignment.stretch,
-//          children: [
-//            Container(
-//              height: 80,
-//              child: Container(
-//                margin: EdgeInsets.all(25),
-//                  child: Text(
-//                    product,
-//                    style: TextStyle(
-//                      color: Colors.white,
-//                      fontSize: 20
-//                    ),)),
-//              decoration: BoxDecoration(
-//                  color: Colors.blue.shade700,
-//                  borderRadius: BorderRadius.only(
-//                      topRight: Radius.circular(10),
-//                      topLeft: Radius.circular(10))),
-//            ),
-//            Container(
-//              height: 40,
-//              child: Container(
-//                child: Row(
-//                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                  children: [
-//                    Text("Number of tasks : 10"),
-//                    Text("Number of customers : 3")
-//                  ],
-//                ),),
-//              decoration: BoxDecoration(
-//                  color: Colors.white,
-//                  borderRadius: BorderRadius.only(
-//                      bottomRight: Radius.circular(10),
-//                      bottomLeft: Radius.circular(10)),
-//                  boxShadow: [
-//                    BoxShadow(
-//                      color: Colors.grey.withOpacity(0.5),
-//                      spreadRadius: 2,
-//                      blurRadius: 4,
-//                      offset: Offset(0, 3), // changes position of shadow
-//                    ),
-//                  ]),
-//            ),
-//          ],
-//        ),
-//      )
-    return Card(
-      elevation: 10,
-      margin: EdgeInsets.fromLTRB(10, 15, 10, 0),
-      child: ListTile(
-          title: Text(
-            product,
-          ),
-          subtitle: Text('A sufficiently long subtitle warrants three lines.'),
-          trailing: Icon(Icons.more_vert),
-          isThreeLine: true,
-          onTap: () {
-            Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (BuildContext context) => ProductDashboard()));
-          }),
-    );
-=======
 class TaskPanel extends StatefulWidget {
 
   @override
@@ -215,10 +141,10 @@ class _TaskPanelState extends State<TaskPanel> {
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
                         Text((() {
-                          if(products[i].taskCount == null){
+                          if(products[i].tasks == null){
                             return "Number of tasks : 0";
                           }
-                            return "Number of tasks : "+products[i].taskCount.toString();
+                            return "Number of tasks : "+products[i].tasks.length.toString();
                         })(),style: TextStyle(fontFamily: 'Arial',)),
                         Text((() {
                           if(products[i].customerIdList == null){
@@ -251,39 +177,25 @@ class _TaskPanelState extends State<TaskPanel> {
 
     return new Column(children: productCardList);
 
->>>>>>> 6549a7d214aedc3b3bc5fde329c0f264a3d0eb09
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlue.shade800,
       appBar: AppBar(
         title:
-<<<<<<< HEAD
-            Text("Product Dashboard", style: TextStyle(color: Colors.black87)),
-        backgroundColor: Colors.white,
-=======
-            Text("Product List", style: TextStyle(color: Colors.white, fontFamily: 'Arial',)),
+        Text("Product Dashboard", style: TextStyle(color: Colors.white, fontFamily: 'Arial',)),
         backgroundColor: Colors.lightBlue.shade800,
->>>>>>> 6549a7d214aedc3b3bc5fde329c0f264a3d0eb09
         shadowColor: Colors.white,
         iconTheme: IconThemeData(
-          color: Colors.black87,
+          color: Colors.white,
         ),
       ),
       drawer: SideDrawer(),
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(height: 10,),
-              productCard("product A", context),
-              productCard("product B", context),
-              productCard("product C", context),
-              productCard("product D", context),
-              productCard("product E", context),
-            ],
-          ),
+          child: productCard(products, context),
         ),
       ),
     );
