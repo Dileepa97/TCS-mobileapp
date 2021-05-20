a4029061e53d5e8cb46c97e31e5f0c52091d5ef8
Kalpa <kalpafernando1998@gmail.com>
2021-05-20 18:39:57 +0530

    fixed path issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
 import 'package:timecapturesystem/services/other/storage_service.dart';
 import 'package:timecapturesystem/view/admin/title_change_management.dart';
 import 'package:timecapturesystem/view/admin/title_management.dart';
@@ -12,13 +13,12 @@ import 'package:timecapturesystem/view/customer/customer_detail_page.dart';
 import 'package:timecapturesystem/view/customer/update_customer_screen.dart';
 import 'package:timecapturesystem/view/lms/admin_leave/absent_users_screens/today_unavailable_users_screen.dart';
 import 'package:timecapturesystem/view/lms/admin_leave/absent_users_screens/week_unavailable_users_screen.dart';
-import 'package:timecapturesystem/view/lms/admin_leave/get_leaves_screens/admin_all_leaves_screen.dart';
 import 'package:timecapturesystem/view/lms/admin_leave/admin_leave_dashboard_screen.dart';
 import 'package:timecapturesystem/view/lms/admin_leave/admin_leave_detail_page.dart';
-import 'package:timecapturesystem/view/lms/admin_leave/get_leaves_screens/admin_leaves_by_status_screen.dart';
 import 'package:timecapturesystem/view/lms/admin_leave/admin_user_leave_related_screens/admin_user_leave_list_screen.dart';
+import 'package:timecapturesystem/view/lms/admin_leave/get_leaves_screens/admin_all_leaves_screen.dart';
+import 'package:timecapturesystem/view/lms/admin_leave/get_leaves_screens/admin_leaves_by_status_screen.dart';
 import 'package:timecapturesystem/view/lms/admin_leave/ongoing_leave_cancellation_manager_screen.dart';
-
 import 'package:timecapturesystem/view/lms/team_leader_leave/TL_today_unavailable_user_screen.dart';
 import 'package:timecapturesystem/view/lms/team_leader_leave/TL_week_unavailable_users_screen.dart';
 import 'package:timecapturesystem/view/lms/user_leave/leave_request/leave_request_confirmation_screen.dart';
@@ -37,6 +37,7 @@ import 'package:timecapturesystem/view/task/product_dashboard.dart';
 import 'package:timecapturesystem/view/user/edit_profile_screen.dart';
 import 'package:timecapturesystem/view/user_management/user_management_dashboard_screen.dart';
 import 'package:timecapturesystem/view/widgets/loading_screen.dart';
+
 import 'managers/orientation.dart';
 import 'view/Auth/login_screen.dart';
 import 'view/Auth/registration_screen.dart';
@@ -45,7 +46,6 @@ import 'view/lms/admin_leave/admin_user_leave_related_screens/admin_user_leave_d
 import 'view/lms/admin_leave/change_allowed_days_screen.dart';
 import 'view/user/pick_image_screen.dart';
 import 'view/user/profile_screen.dart';
-import 'package:flutter_downloader/flutter_downloader.dart';
 
 void main() async {
   await DotEnv().load('.env');
@@ -64,7 +64,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterDownloader.initialize(
       debug: true // optional: set false to disable printing logs to console
-  );
+      );
 
   runApp(MyApp(userData, username));
 }
@@ -89,93 +89,78 @@ class _MyAppState extends State<MyApp> {
     return FutureBuilder<dynamic>(
         future: (TokenStorageService.authDataOrEmpty),
         builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          var routes;
+          var routes = {
+            HomePage.id: (context) => HomePage(),
+            LoadingScreen.id: (context) => LoadingScreen(),
+            UserManagementDashboard.id: (context) => UserManagementDashboard(),
+            LoginScreen.id: (context) => LoginScreen(),
+            RegistrationScreen.id: (context) => RegistrationScreen(),
+            Profile.id: (context) => Profile(),
+            EditProfile.id: (context) => EditProfile(),
+            ChangePasswordScreen.id: (context) => ChangePasswordScreen(),
+            PickImageScreen.id: (context) => PickImageScreen(),
+            TitleManagementScreen.id: (context) => TitleManagementScreen(),
+            TitleChangeManagementScreen.id: (context) =>
+                TitleChangeManagementScreen(),
+            ForgotPasswordScreen.id: (context) => ForgotPasswordScreen(),
+            ForgotPasswordChangeScreen.id: (context) =>
+                ForgotPasswordChangeScreen(),
+            NotificationCenter.id: (context) => NotificationCenter(),
+
+            ///routes leave
+            ///Team member leaves
+            UserLeaveDashboard.id: (context) => UserLeaveDashboard(),
+            FirstRequestScreen.id: (context) => FirstRequestScreen(),
+            LeaveRequestMainScreen.id: (context) => LeaveRequestMainScreen(),
+            RequestConfirmationScreen.id: (context) =>
+                RequestConfirmationScreen(),
+            OwnUserLeaves.id: (context) => OwnUserLeaves(),
+            UserLeaveAvailable.id: (context) => UserLeaveAvailable(),
+            MoreLeaveDetails.id: (context) => MoreLeaveDetails(),
+            UserLeaveDetailsPage.id: (context) => UserLeaveDetailsPage(),
+
+            //Admin leaves
+            AdminLeaveDashBoard.id: (context) => AdminLeaveDashBoard(),
+            AdminAllLeaves.id: (context) => AdminAllLeaves(),
+            AdminLeaveByStatus.id: (context) => AdminLeaveByStatus(),
+            TodayUnavailableUserScreen.id: (context) =>
+                TodayUnavailableUserScreen(),
+            WeekUnavailableUserScreen.id: (context) =>
+                WeekUnavailableUserScreen(),
+            AdminLeaveDetailsPage.id: (context) => AdminLeaveDetailsPage(),
+            ChangeAllowedDays.id: (context) => ChangeAllowedDays(),
+            OngoingLeaveCancellationManager.id: (context) =>
+                OngoingLeaveCancellationManager(),
+            AdminUserLeaveListScreen.id: (context) =>
+                AdminUserLeaveListScreen(),
+
+            ///Team leader leaves
+            TLTodayUnavailableUserScreen.id: (context) =>
+                TLTodayUnavailableUserScreen(),
+            TLWeekUnavailableUserScreen.id: (context) =>
+                TLWeekUnavailableUserScreen(),
+
+            ///Product
+            ProductManagementDashboard.id: (context) =>
+                ProductManagementDashboard(),
+            ProductDetailPage.id: (context) => ProductDetailPage(),
+            AddProductScreen.id: (context) => AddProductScreen(),
+            UpdateProductScreen.id: (context) => UpdateProductScreen(),
+
+            ///task
+            ProductDashboard.id: (context) => ProductDashboard(),
+
+            ///customer
+            CustomerDashboard.id: (context) => CustomerDashboard(),
+            AddCustomerScreen.id: (context) => AddCustomerScreen(),
+            UpdateCustomerScreen.id: (context) => UpdateCustomerScreen(),
+            CustomerDetailPage.id: (context) => CustomerDetailPage(),
+          };
           var initialRoute;
           if (userData != null) {
-            //TODO: check if expired and resolve bug
             initialRoute = '/';
-            routes = {
-              //add routes that user can access only after logging
-              HomePage.id: (context) => HomePage(),
-              LoadingScreen.id: (context) => LoadingScreen(),
-              UserManagementDashboard.id: (context) =>
-                  UserManagementDashboard(),
-              LoginScreen.id: (context) => LoginScreen(),
-              RegistrationScreen.id: (context) => RegistrationScreen(),
-              Profile.id: (context) => Profile(),
-              EditProfile.id: (context) => EditProfile(),
-              ChangePasswordScreen.id: (context) => ChangePasswordScreen(),
-              PickImageScreen.id: (context) => PickImageScreen(),
-              TitleManagementScreen.id: (context) => TitleManagementScreen(),
-              TitleChangeManagementScreen.id: (context) =>
-                  TitleChangeManagementScreen(),
-              ForgotPasswordScreen.id: (context) => ForgotPasswordScreen(),
-              ForgotPasswordChangeScreen.id: (context) =>
-                  ForgotPasswordChangeScreen(),
-              NotificationCenter.id: (context) => NotificationCenter(),
-
-              ///routes leave
-              ///Team member leaves
-              UserLeaveDashboard.id: (context) => UserLeaveDashboard(),
-              FirstRequestScreen.id: (context) => FirstRequestScreen(),
-              LeaveRequestMainScreen.id: (context) => LeaveRequestMainScreen(),
-              RequestConfirmationScreen.id: (context) =>
-                  RequestConfirmationScreen(),
-              OwnUserLeaves.id: (context) => OwnUserLeaves(),
-              UserLeaveAvailable.id: (context) => UserLeaveAvailable(),
-              MoreLeaveDetails.id: (context) => MoreLeaveDetails(),
-              UserLeaveDetailsPage.id: (context) => UserLeaveDetailsPage(),
-
-              //Admin leaves
-              AdminLeaveDashBoard.id: (context) => AdminLeaveDashBoard(),
-              AdminAllLeaves.id: (context) => AdminAllLeaves(),
-              AdminLeaveByStatus.id: (context) => AdminLeaveByStatus(),
-              TodayUnavailableUserScreen.id: (context) =>
-                  TodayUnavailableUserScreen(),
-              WeekUnavailableUserScreen.id: (context) =>
-                  WeekUnavailableUserScreen(),
-              AdminLeaveDetailsPage.id: (context) => AdminLeaveDetailsPage(),
-              ChangeAllowedDays.id: (context) => ChangeAllowedDays(),
-              OngoingLeaveCancellationManager.id: (context) =>
-                  OngoingLeaveCancellationManager(),
-              AdminUserLeaveListScreen.id: (context) =>
-                  AdminUserLeaveListScreen(),
-
-              ///Team leader leaves
-              TLTodayUnavailableUserScreen.id: (context) =>
-                  TLTodayUnavailableUserScreen(),
-              TLWeekUnavailableUserScreen.id: (context) =>
-                  TLWeekUnavailableUserScreen(),
-
-              ///Product
-              ProductManagementDashboard.id: (context) =>
-                  ProductManagementDashboard(),
-              ProductDetailPage.id: (context) => ProductDetailPage(),
-              AddProductScreen.id: (context) => AddProductScreen(),
-              UpdateProductScreen.id: (context) => UpdateProductScreen(),
-
-              ///task
-              ProductDashboard.id: (context) => ProductDashboard(),
-
-              ///customer
-              CustomerDashboard.id: (context) => CustomerDashboard(),
-              AddCustomerScreen.id: (context) => AddCustomerScreen(),
-              UpdateCustomerScreen.id: (context) => UpdateCustomerScreen(),
-              CustomerDetailPage.id: (context) => CustomerDetailPage(),
-            };
           } else {
             initialRoute = LoginScreen.id;
-            //add routes that user can access without login
-            routes = {
-              '/': (context) => HomePage(),
-              LoginScreen.id: (context) => LoginScreen(
-                  // username: username,
-                  ),
-              ForgotPasswordScreen.id: (context) => ForgotPasswordScreen(),
-              RegistrationScreen.id: (context) => RegistrationScreen(),
-              ForgotPasswordChangeScreen.id: (context) =>
-                  ForgotPasswordChangeScreen(),
-            };
           }
 
           return MaterialApp(
